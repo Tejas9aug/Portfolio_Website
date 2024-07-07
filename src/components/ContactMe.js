1e2c44dbbb1aafe721b44da6f2f964ef7c686b64
Tejas9aug <122152847+Tejas9aug@users.noreply.github.com>
2024-07-07 23:13:48 +0530

    changes done

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,9 +1,110 @@
-import React from 'react'
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
-export default function ContactMe() {
+export default function ContactMe(props) {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefalult();
+
+    emailjs.sendForm("service_re7eevo", "template_zg1rdpw", form.current);
+    e.target.reset();
+  };
+
   return (
-    <div>
-      This is contact me.
-    </div>
-  )
+    <section
+      className={props.bg ? "contSecW cSec" : "contSecB cSec"}
+      id="ContactMe"
+    >
+      <h2 className="contTitle">
+        {" "}
+        <u>Contact Me</u>
+      </h2>
+
+      <div className="contContainer">
+        <div className="contContent">
+          <h3 className="contTitle">
+            <u>Get in touch</u>
+          </h3>
+          <div className="contCont">
+            <div className={props.bg ? "contInfoW" : "contInfoB"}>
+              <div className="contCard">
+                <h3 className="contCardTitle">
+                  Email
+                  <i class="uil uil-envelope-check contact__card-icon"></i>
+                </h3>
+                <div className="contNameDir">
+                  <a
+                    href="mailto:tejaspatange2025@gmail.com"
+                    className="contBtn"
+                    target="__blank"
+                  >
+                    <span className="contCardData">Reach out</span>
+                    <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+                  </a>
+                </div>
+              </div>
+              <div className="contCard">
+                <h3 className="contCardTitle">
+                  Linkedin
+                  <i class="uil uil-linkedin contact__card-icon"></i>
+                </h3>
+                <div className="contNameDir">
+                  <a
+                    href="linkedin.com/in/tejas-patange-bb700b229/"
+                    className="contBtn"
+                    target="__blank"
+                  >
+                    <span className="contCardData">Tejas Patange</span>
+                    <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="contContent">
+          <h3 className="contTitle"><u>Send a Message</u></h3>
+          <div className="contCont">
+            <form ref = {form} onSubmit={sendEmail} className="contForm" >
+              <div className="contFormDiv" >
+              <label className={props.bg ? 'contFormTagW' : 'contFormTagB'}>Name: </label>
+                 <input 
+                 type="text"
+                 name="name"
+                 className={props.bg?'contFormInW' : 'contFormInB'}
+                 placeholder="Your Name"
+                 />
+
+              </div>
+
+              <div className='contFormDiv'>
+              <label className={props.bg?'contFormTagW':'contFormTagB'}>Mail: </label>
+              <input 
+               type="email" 
+               name="email" 
+               className={props.bg?"contFormInW":"contFormInB"} 
+               placeholder="Your Mail"></input>
+            </div>
+
+            <div className='contFormDiv contFormArea'>
+              <label className={props.bg?'contFormTagW':'contFormTagB'}>Message</label>
+              <textarea 
+              name="message" 
+              cols="30" rows="10" 
+              className={props.bg?"contFormInW":"contFormInB"} 
+              placeholder='Your Message'></textarea>
+            </div>
+
+            <button className='formSubmit'>Send
+            <i className="uil uil-message" ></i>
+            </button>
+
+            </form>
+
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,43 @@
 import React from 'react'
 
-export default function Footer() {
+export default function Footer(props) {
   return (
-    <div>
-      This is footer
-    </div>
+    <section className={props.bg ? 'footerSecW secFooter' : 'footerSecB secFooter'} id='Footer'>
+      <footer className='footer'>
+        <div className='footerContainer'>
+          <h1 className='footerTitle'>Tejas Patange</h1>
+
+          <ul className='footerList'>
+            <li>
+              <a href="#About" className='footerLinks'>About</a>
+            </li>
+
+            <li>
+              <a href="#Qualifications" className='footerLinks'>Qualifications</a>
+            </li>
+
+            <li>
+              <a href="#Projects" className='footerLinks'>Projects</a>
+            </li>
+          </ul>
+
+          <div className='footerSocial'>
+            <a href='inkedin.com/in/tejas-patange-bb700b229/' className='footerIcon' target='_blank'>
+              <i class="uil uil-linkedin-alt"></i>
+            </a>
+
+            <a href='https://github.com/Tejas9aug' className='footerIcon' target='_blank'>
+              <i class="uil uil-github"></i>
+            </a>
+
+           
+          </div>
+
+          <span className='footerCopyright'>
+           &#169; 2024 - Tejas Patange. All rights reserved.
+          </span>
+        </div>
+      </footer>
+    </section>
   )
 }
